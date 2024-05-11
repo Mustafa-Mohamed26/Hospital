769cc180d7e64bee3f223d0b29bd1562e6e6e50b
Mustafa Mohamed Ali <151867905+Mustafa-Mohamed26@users.noreply.github.com>
2024-05-12 01:47:12 +0300

    blog

---
+++ blog.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Blog</title>
+        <link rel="stylesheet" href="./css/navbar.css">
+        <link rel="stylesheet" href="./css/footer.css">
+        <link rel="stylesheet" href="./css/blog.css">
+        <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+        />
+    </head>
+    <body>
+        <header>
+            <nav>
+                <div class="upNav">
+                    <div class="title left">
+                        <p class="small">EMERGENCY CASES</p>
+                        <p class="big">+1(800) 450 7550</p>
+                    </div>
+
+                    <div class="img">
+                        <a href="./index.php"><img src="./images/logo.png" alt="logo"></a>
+                    </div>
+
+                    <div class="title right">
+                    <p class="small">ONLINE CONSULTATION</p>
+                    <p class="big">VALEO@GMAIL.COM</p>
+                    </div>
+                </div>
+
+                <div class="midNav">
+                    <ul>
+                        <li ><a href="./index.php">HOME</a></li>
+                        <li ><a href="./aboutUs.php">ABOUT US</a></li>
+                        <li ><a href="./Team.php">TEAM</a></li>
+                        <li ><a href="./service.php">SERVICES</a></li>
+                        <li ><a href="./TimeTable.php">TIMETABLE</a></li>
+                        <li ><a href="./blog.php">BLOG</a></li>
+                    </ul>
+
+                    <div class="reg">
+                        <p><a href="./logIn.php">Login</a> / <a href="./signUp.php">Sign Up</a></p>
+                    </div>
+                </div>
+
+                <div class="bottomNav">
+                    <a href="./index.php">HOME PAGE</a>
+                    <p> / BLOG</p>
+                </div>
+            </nav>
+        </header>
+
+        <section>
+            <div class="container">
+                <img src="./images/Depositphotos_1.jpg" alt="">
+                <div class="content">
+                    <div class="title">
+                        <p id="first">BY EMMAJOHNSON ON JULY 6, 2019</p>
+                        <p id="second">MEDICAL TREATMENT</p>
+                    </div>
+
+                    <h1>The Earlier You Start, the Better</h1>
+                    <p id="third">It’s not uncommon for children who have obesity to become overweight adults. Teaching children healthy habits earlier encourages them to carry these healthy habits into adulthood. Not only will the child be healthier, but they’ll learn the importance of proper discipline and healthy habits when it comes to food and exercise.</p>
+                </div>
+            </div>
+
+            <div class="container">
+                <img src="./images/Depositphotos_2.jpg" alt="">
+                <div class="content">
+                    <div class="title">
+                        <p id="first">BY EMMAJOHNSON ON JULY 6, 2019</p>
+                        <p id="second">LABORATORY TEST</p>
+                    </div>
+
+                    <h1>Greatly Improves Flexibility</h1>
+                    <p id="third">With summer in full swing, everyone is out exploring, adventuring, and being active in their daily life. However, stretching after their physical activity is often overlooked. Many people forget about stretching, even those that are normally considered “active” individuals. Developing good stretching habits can help to improve flexibility and performance, especially in sports and other physical activities.</p>
+                </div>
+            </div>
+
+            <div class="container">
+                <img src="./images/Depositphotos_3.jpg" alt="">
+                <div class="content">
+                    <div class="title">
+                        <p id="first">BY EMMAJOHNSON ON JULY 6, 2019</p>
+                        <p id="second">GENERAL ANALYSIS</p>
+                    </div>
+
+                    <h1>Reduces the Risk of Injury</h1>
+                    <p id="third">Stretching safely exposes your body to the strenuous limits that your sport or activity may introduce. By stretching in advance, it “prepares” the body for meeting those limits as they occur by loosening up the muscles slowly and carefully. Without stretching, if your muscle is forced to endure certain limits (typically very quickly and strongly), it can cause damage to the muscles. Stretching in advance will reduce the risk of injury because the muscle is already “prepared” for that exertion of force.</p>
+                </div>
+            </div>
+
+            <div class="container">
+                <img src="./images/things.jpg" alt="">
+                <div class="content">
+                    <div class="title">
+                        <p id="first">BY EMMAJOHNSON ON JULY 6, 2019</p>
+                        <p id="second">CARDIO MONITORING</p>
+                    </div>
+
+                    <h1>Improves Flexibility</h1>
+                    <p id="third">With summer in full swing, everyone is out exploring, adventuring, and being active in their daily life. However, stretching after their physical activity is often overlooked. Many people forget about stretching, even those that are normally considered “active” individuals. Developing good stretching habits can help to improve flexibility and performance, especially in sports and other physical activities.</p>
+                </div>
+            </div>
+            
+        </section>
+        
+        <footer>
+            <div class="upperF">
+                <div class="title left white">
+                    <p class="small">EMERGENCY CASES</p>
+                    <p class="big">+1(800) 450 7550</p>
+                </div>
+
+                <div class="img">
+                    <a href="./index.php"><img src="./images/logo_alt.png" alt="logo"></a>
+                </div>
+
+                <div class="title right white">
+                    <p class="small">ONLINE CONSULTATION</p>
+                    <p class="big">VALEO@GMAIL.COM</p>
+                </div>
+            </div>
+
+            <div class="midF">
+                <div class="slide s1">
+                    <h1>NEWSLETTER SIGNUP</h1>
+                    <a href="./signUp.php"> Sign Up</a>
+                </div>
+
+                <div class="slide s2">
+                    <h1>SEARCH ON THE MAP</h1>
+                    <a href="#">GET DIRECTIONS ON THE MAP</a>
+                </div>
+
+                <div class="slide s3">
+                    <h1>MEET OUR DOCTORS</h1>
+                    <a href="./TimeTable.php">DOCTORS TIMETABLE</a>
+                </div>
+            </div>
+
+            <div class="lowerF">
+                <p>© VALEO - MEDICAL CLINIC / HEALTH CENTER © 2024</p>
+                <div class="social-links">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+                <p>CREATED WITH BY AUTHOR</p>
+            </div>
+        </footer>
+    </body>
+</html>
